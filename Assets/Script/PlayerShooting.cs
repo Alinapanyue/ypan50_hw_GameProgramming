59508c2d1be38d3086ea8231312dfe75346d701e
Alina Pan <alinapanyue@outlook.com>
2025-02-07 22:33:13 -0500

    optimized OnFire in PlayerShooting.cs

--- Assets/Script/PlayerShooting.cs
+++ Assets/Script/PlayerShooting.cs
@@ -8,10 +8,13 @@ public class PlayerShooting : MonoBehaviour
     public GameObject prefab;
     public GameObject shootPoint;
     
-    public void OnFire(){
+    public void OnFire(InputValue value){
+        if (value.isPressed)
+    {
         GameObject clone = Instantiate(prefab);
         clone.transform.position = shootPoint.transform.position;
         clone.transform.rotation = shootPoint.transform.rotation;
+    }
     }
 
     
